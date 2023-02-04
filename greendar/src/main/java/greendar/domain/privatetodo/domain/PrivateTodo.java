a3283ad6064532f982d3b0946fa04d9559a83805
chosunghyun18 <krismon@naver.com>
2023-02-04 20:04:00 +0900

    feat : add the generation type in the id

--- greendar/src/main/java/greendar/domain/privatetodo/domain/PrivateTodo.java
+++ greendar/src/main/java/greendar/domain/privatetodo/domain/PrivateTodo.java
@@ -7,9 +7,12 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,10 +22,11 @@ import lombok.Setter;
 public class PrivateTodo {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "private_todo_id") //pk
     private Long id ;
     private String task;
+    @Temporal(TemporalType.DATE)
     private Date date;
     @JsonIgnore
     private String imageUrl;
