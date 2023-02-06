d670ec1b9a38f6da850420d8abab6cc9d5f39811
chosunghyun18 <krismon@naver.com>
2023-02-06 16:46:18 +0900

    add : add privatetodo

--- greendar/src/main/java/greendar/domain/privatetodo/api/PrivateTodoApi.java
+++ greendar/src/main/java/greendar/domain/privatetodo/api/PrivateTodoApi.java
@@ -1,4 +1,36 @@
 package greendar.domain.privatetodo.api;
 
+import greendar.domain.member.application.MemberService;
+import greendar.domain.member.domain.Member;
+import greendar.domain.privatetodo.application.PrivateTodoService;
+import greendar.domain.privatetodo.domain.PrivateTodo;
+import greendar.domain.privatetodo.dto.PrivateTodoDtos.PrivateTodoRequestDto;
+import greendar.global.common.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class PrivateTodoApi {
+
+    private final MemberService memberService;
+    private final PrivateTodoService privateTodoService;
+
+    @PostMapping(value = "/private/todo")
+    public ApiResponse addPrivateTodo(@RequestHeader("Authorization") String member_token,
+                                      @RequestBody PrivateTodoRequestDto request){
+
+        Member member = memberService.findOne();
+
+        PrivateTodo privateTodo = privateTodoService.saveTodo(member,request.getTask(),request.getDate());
+
+        return  ApiResponse.success("",privateTodo);
+    }
+
+
+
+
 }
---
+++ greendar/src/main/java/greendar/domain/privatetodo/dto/PrivateTodoDtos.java
@@ -0,0 +1,17 @@
+package greendar.domain.privatetodo.dto;
+
+import java.rmi.registry.LocateRegistry;
+import java.time.LocalDate;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class PrivateTodoDtos {
+    @Data
+    @NoArgsConstructor
+    public static class PrivateTodoRequestDto
+    {
+        String task;
+        LocalDate date;
+
+    }
+}
