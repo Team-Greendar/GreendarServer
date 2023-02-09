ef2f14a9dae38cf8dd9850bbf5c40a2371fe6f1d
chosunghyun18 <krismon@naver.com>
2023-02-09 15:35:28 +0900

    feat : add monthly get all api for check

--- greendar/src/main/java/greendar/domain/privatetodo/api/PrivateTodoApi.java
+++ greendar/src/main/java/greendar/domain/privatetodo/api/PrivateTodoApi.java
@@ -5,6 +5,7 @@ import greendar.domain.member.domain.Member;
 import greendar.domain.privatetodo.application.PrivateTodoService;
 import greendar.domain.privatetodo.dao.DailyAchievementRateDao;
 import greendar.domain.privatetodo.domain.PrivateTodo;
+import greendar.domain.privatetodo.dto.PrivateTodoDtos.PrivateTodoImagePutRequestDto;
 import greendar.domain.privatetodo.dto.PrivateTodoDtos.PrivateTodoPostRequestDto;
 import greendar.domain.privatetodo.dto.PrivateTodoDtos.PrivateTodoResponse;
 import greendar.global.common.ApiResponse;
@@ -12,6 +13,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,32 +50,37 @@ public class PrivateTodoApi {
         return  ApiResponse.success( new PrivateTodoResponse(privateTodo));
     }
     //get privateTodo Id and  set image
-    @PutMapping(value = "/private/todo/image")
-    public ApiResponse setPrivateTodoImageUrl(@RequestHeader("Authorization") Long member_token,
-                                              @RequestBody PrivateTodoPostRequestDto request) {
 
-        Member member = memberService.findOne(member_token);
+//    public ApiResponse setPrivateTodoImageUrl(@RequestHeader("Authorization") Long member_token,
+//                                      @RequestBody PrivateTodoImagePutRequestDto request) {
+//        Member member = memberService.findOne(member_token);
+//        PrivateTodo result = privateTodoService.saveTodo(member,request.getTask(),request.getDate());
+//        return  ApiResponse.success(result);
+//    }
 
-        PrivateTodo result = privateTodoService.saveTodo(member,request.getTask(),request.getDate());
-
-        return  ApiResponse.success(result);
-    }
-
-    // need Refactoring
     @GetMapping(value = "/private/todo/{date}")
     public ApiResponse getPrivateTodoByDate(@RequestHeader("Authorization") Long member_token,
-                                            @PathVariable LocalDate date) {
+                                            @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd")  LocalDate date) {
         Member member = memberService.findOne(member_token);
         List<PrivateTodo> result =  privateTodoService.getAllPrivateTodoByOneDay(date,member);
-        return ApiResponse.success(result);
+        List<PrivateTodoResponse> collect = result.stream()
+                .map(r->new PrivateTodoResponse(r))
+                .collect(Collectors.toList());
+        if(collect.isEmpty()) return ApiResponse.success("None");
+        return ApiResponse.success(collect);
     }
 
     @GetMapping(value = "/private/todo/monthly/{date}")
     public ApiResponse getPrivateTodoByMonthlyDate(@RequestHeader("Authorization") Long member_token,
-                                                   @PathVariable LocalDate date) {
+                                                   @PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
+        System.out.println(date);
         Member member = memberService.findOne(member_token);
         List<PrivateTodo> result =  privateTodoService.getAllPrivateTodoByOneMonth(date,member);
-        return ApiResponse.success(result);
+        System.out.println(result);
+        List<PrivateTodoResponse> collect = result.stream()
+                .map(r->new PrivateTodoResponse(r))
+                .collect(Collectors.toList());
+        return ApiResponse.success(collect);
     }
 
     @GetMapping(value = "/private/todo/monthly/ratio/{date}")
