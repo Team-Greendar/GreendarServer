6f8a59cc70d89fcb69942fbfb1a34f4575817f7a
chosunghyun18 <krismon@naver.com>
2023-04-18 22:43:50 +0900

    feat : add query find by date

--- greendar/src/main/java/greendar/domain/eventtodo/dao/EventTodoRepository.java
+++ greendar/src/main/java/greendar/domain/eventtodo/dao/EventTodoRepository.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -18,7 +19,16 @@ public interface EventTodoRepository extends JpaRepository<EventTodo,Long> {
 
     List<EventTodo> findAllEventTodoByMember(Long memberId);
 
+    @Query("select  new greendar.domain.eventtodo.dto.EventTodoResponseDto(e) "+
+            "from EventTodo e " +
+            "where e.eventTodoItem.date = :oneDay and e.member.id = :memberId " +
+            "order by e.eventTodoItem.date desc")
     List<EventTodoResponseDto> findAllByDay(LocalDate day, Member member);
+
+    @Query("select new greendar.domain.eventtodo.dto.EventTodoResponseDto(e) " +
+            "from EventTodo e " +
+            "where e.member.id=:memberId and e.eventTodoItem.date between :startDate and :endDate " +
+            "order by  e.eventTodoItem.date desc")
     List<EventTodoResponseDto> findAllByMonth(LocalDate date, Member member);
 
 }
